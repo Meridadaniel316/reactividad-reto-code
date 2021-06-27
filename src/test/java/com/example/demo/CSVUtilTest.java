7a9b091d116c3376a6734920809aa9892c426e88
Merida-Daniel <dacastamerd@gmail.com>
2021-06-27 14:22:11 -0500

    Se agrego el test de filtrar jugadores por nacionalidadRacking

--- src/test/java/com/example/demo/CSVUtilTest.java
+++ src/test/java/com/example/demo/CSVUtilTest.java
@@ -42,45 +42,68 @@ public class CSVUtilTest {
     }
 
 
-    @Test
+    @Test //TEST PARA VER SI SI FILTRA A TODOS LOS JUGADORES MAYORES DE 35 AÑOS, TODO FUNCIONA CORRECTO
     void reactive_filtrarJugadoresMayoresA35(){
         List<Player> list = CsvUtilFile.getPlayers();
         Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
         Mono<Map<String, Collection<Player>>> listFilter = listFlux
                 .filter(player -> player.age >= 35)
-                .map(player -> {
-                    player.name = player.name.toUpperCase(Locale.ROOT);
-                    return player;
-                })
-                .buffer(100)
-                .flatMap(playerA -> listFlux
-                         .filter(playerB -> playerA.stream()
-                                 .anyMatch(a ->  a.club.equals(playerB.club)))
-                )
                 .distinct()
                 .collectMultimap(Player::getClub);
 
-        assert listFilter.block().size() == 1;
+        // ============================== TEST VISUAL
+        Objects.requireNonNull(listFilter.block()).forEach((clubTeam, players) -> {
+            System.out.println("====================");
+            System.out.println("Equipo: " + (clubTeam.isEmpty() ? "DESCONOCIDO" : clubTeam));
+            System.out.println("====================");
+            players.forEach(player -> {
+                System.out.println(player.name +" |-| "+ player.age);
+            });
+        });
+        assert Objects.requireNonNull(listFilter.block()).size() == 322;
     }
 
-    @Test
-    void reactive_filtrarJugadores34(){
+    @Test //TEST PARA FILTRAR JUGADORES MAYORES A 34 AÑOS, POR EQUIPOS, TODO FUNCIONA CORRECTAMENTE
+    void reactive_filtrarJugadoresMayores34(){
         List<Player> list = CsvUtilFile.getPlayers();
         Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
         Mono<Map<String, Collection<Player>>> listFilter = listFlux
-                .filter(player -> player.club.equals("Juventus"))
+                .filter(player -> player.club.equals(""))
                 .filter(player -> player.age >= 34)
                 .distinct()
                 .collectMultimap(Player::getClub);
 
+        // ============================== TEST VISUAL
         Objects.requireNonNull(listFilter.block()).forEach((clubTeam, players) -> {
-            System.out.println("EQUIPO: "+clubTeam);
+            System.out.println("Equipo: " + (clubTeam.isEmpty() ? "DESCONOCIDO" : clubTeam));
             players.forEach(player -> {
                 System.out.println(player.name +" |-| "+ player.age);
-                assert player.club.equals("Juventus");
             });
         });
         assert Objects.requireNonNull(listFilter.block()).size() == 1;
     }
 
+    @Test
+    void reactive_filtrarNacionalidadYVictoriasJugadores() {
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<Player>>> listFilter = listFlux
+                .buffer(100)
+                .flatMap(playerA -> listFlux
+                        .filter(playerB -> playerA.stream()
+                                .anyMatch(a -> a.national.equals(playerB.national)))
+                ).distinct()
+                .sort((country,player)-> player.winners)
+                .collectMultimap(Player::getNational);
+
+        Objects.requireNonNull(listFilter.block()).forEach((country, players)->{
+            System.out.println("====================");
+            System.out.println("PAIS: " + (country.isEmpty() ? "DESCONOCIDO" : country));
+            System.out.println("====================");
+            players.forEach(player -> {
+                System.out.println(player.name + " |-| "+player.winners);
+            });
+        });
+
+    }
 }
